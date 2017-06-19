f4b468193babc2052bcaf14f3a384e7d2ff5b810
natedaag <natedaage@yahoo.com>
2017-06-19 11:18:30 -0600

    prints without commas

--- src/edu/cnm/deepdive/security/WordList.java
+++ src/edu/cnm/deepdive/security/WordList.java
@@ -7,13 +7,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.Random;
 
 public class WordList {
 
 	public static final int RECOMMENDED_PHRASE_LENGTH = 5;
-	
+
 	public static final String WORD_LIST_FILE = "resources/eff_large_wordlist.txt";
-	
+
 	private static final String PROPERTIES_FILE = "resources/text.properties";
 
 	private static String usageMessage = "";
@@ -26,13 +27,14 @@ public class WordList {
 			int phraseLength = (args.length > 0) ? Integer.parseInt(args[0]) : RECOMMENDED_PHRASE_LENGTH;
 			if (phraseLength <= 0) {
 				throw new IllegalArgumentException(errorMessage);
-			} // end if 
+			} // end if
 			else if (phraseLength < RECOMMENDED_PHRASE_LENGTH) {
 				System.out.println(warningMessage);
 			} // end else-if
-		String[] wordList = getWordList(WORD_LIST_FILE);
-		System.out.println(Arrays.toString(wordList)); // FIXME - Get rid of this debugging.
-			} catch (NumberFormatException ex) {
+			String[] wordList = getWordList(WORD_LIST_FILE);
+			String[] selectedWords = getRandomWords(phraseLength, wordList);
+			System.out.println(getJoinedString(selectedWords));
+		} catch (NumberFormatException ex) {
 			ex.printStackTrace();
 			System.out.println(errorMessage);
 			System.out.printf(usageMessage, WordList.class.getName());
@@ -44,11 +46,10 @@ public class WordList {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			System.exit(1);
-		} //end catch IOE
+		} // end catch IOE
 	} // end main method
 
-	private static void loadResources()  
-			throws IOException {
+	private static void loadResources() throws IOException {
 		Properties properties = new Properties();
 		try (InputStream input = WordList.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
 			properties.load(input);
@@ -57,18 +58,35 @@ public class WordList {
 			warningMessage = properties.getProperty("warning.message");
 		}
 	}
-	
-	public static String[] getWordList (String listPath) 
-	throws IOException {
-		try (BufferedReader reader 
-				= new BufferedReader(
-						new InputStreamReader(
-								WordList.class.getClassLoader().getResourceAsStream(listPath)))) {
-			ArrayList<String> words = new ArrayList <> ();
-			for (String line = reader.readLine(); line != null; line = reader.readLine()){
+
+	public static String[] getWordList(String listPath) throws IOException {
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(WordList.class.getClassLoader().getResourceAsStream(listPath)))) {
+			ArrayList<String> words = new ArrayList<>();
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
 				words.add(line.split("\\s+")[1]);
 			}
-			return words.toArray(new String[]{});
+			return words.toArray(new String[] {});
 		}
 	}
+
+	public static String[] getRandomWords(int numWords, String[] wordList) {
+		String[] selection = new String [numWords];
+		Random rng = new Random();
+		for (int i = 0; i < selection.length; i++) {
+			int selectedPosition = rng.nextInt(wordList.length);
+			selection[i] = wordList[selectedPosition];
+		}
+		return selection;
+		}	
+	private static String getJoinedString(String[] source) {
+		StringBuilder builder = new StringBuilder();
+		for (String item : source) {
+			builder.append(item);
+			builder.append(" ");
+			
+		}
+		return builder.toString().trim();
+	}
+
 }
