e668b66912cade6f2703c9b5d0764e157234cab2
natedaag <natedaage@yahoo.com>
2017-06-19 09:51:32 -0600

    Created array.  Printed wordlist to array.

--- src/edu/cnm/deepdive/security/WordList.java
+++ src/edu/cnm/deepdive/security/WordList.java
@@ -1,12 +1,18 @@
 package edu.cnm.deepdive.security;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class WordList {
 
 	public static final int RECOMMENDED_PHRASE_LENGTH = 5;
+	
+	public static final String WORD_LIST_FILE = "resources/eff_large_wordlist.txt";
 	
 	private static final String PROPERTIES_FILE = "resources/text.properties";
 
@@ -20,10 +26,12 @@ public class WordList {
 			int phraseLength = (args.length > 0) ? Integer.parseInt(args[0]) : RECOMMENDED_PHRASE_LENGTH;
 			if (phraseLength <= 0) {
 				throw new IllegalArgumentException(errorMessage);
-			} 
+			} // end if 
 			else if (phraseLength < RECOMMENDED_PHRASE_LENGTH) {
 				System.out.println(warningMessage);
-			}
+			} // end else-if
+		String[] wordList = getWordList(WORD_LIST_FILE);
+		System.out.println(Arrays.toString(wordList)); // FIXME - Get rid of this debugging.
 			} catch (NumberFormatException ex) {
 			ex.printStackTrace();
 			System.out.println(errorMessage);
@@ -36,10 +44,10 @@ public class WordList {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			System.exit(1);
-		}
-	}
+		} //end catch IOE
+	} // end main method
 
-	private static void loadResources() 
+	private static void loadResources()  
 			throws IOException {
 		Properties properties = new Properties();
 		try (InputStream input = WordList.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
@@ -47,6 +55,20 @@ public class WordList {
 			usageMessage = properties.getProperty("usage.message");
 			errorMessage = properties.getProperty("error.message");
 			warningMessage = properties.getProperty("warning.message");
+		}
+	}
+	
+	public static String[] getWordList (String listPath) 
+	throws IOException {
+		try (BufferedReader reader 
+				= new BufferedReader(
+						new InputStreamReader(
+								WordList.class.getClassLoader().getResourceAsStream(listPath)))) {
+			ArrayList<String> words = new ArrayList <> ();
+			for (String line = reader.readLine(); line != null; line = reader.readLine()){
+				words.add(line.split("\\s+")[1]);
+			}
+			return words.toArray(new String[]{});
 		}
 	}
 }
