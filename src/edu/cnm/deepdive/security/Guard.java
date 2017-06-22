2117819213fb22c963bdc08963523d5d059ce173
natedaag <natedaage@yahoo.com>
2017-06-22 13:59:21 -0600

    debugging session #1

--- src/edu/cnm/deepdive/security/Guard.java
+++ src/edu/cnm/deepdive/security/Guard.java
@@ -3,6 +3,9 @@
  */
 package edu.cnm.deepdive.security;
 
+import java.util.Arrays;
+
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -27,33 +30,38 @@ public class Guard {
 	 *            Command line arguments specifying generation options.
 	 */
 	public static void main(String[] args) {
-		Option[] options = getOptions(args);
-		String artifact = generateArtifact(options);
+		CommandLine cmdLine = getOptions(args);
+		String artifact = generateArtifact(cmdLine);
 		emitArtifact(artifact);
 	} // end main method
 
-	static Option[] getOptions(String[] args) {
-		try {		Option lengthOption = Option.builder("L").argName("length")
+	static CommandLine getOptions(String[] args) {
+		try {		Option lengthOption = Option.builder("l").argName("length")
 				 				   			 .hasArg()
 				 							 .longOpt("length")
 				 							 .numberOfArgs(1)
+				 							 .type(Integer.class)
 				 							 .build();
 		Option delimiterOption = Option.builder("d").argName("delimiter")
 												   .hasArg()
 												   .longOpt("delimiter")
 												   .numberOfArgs(1)
 												   .optionalArg(true)
+												   .type(String.class)
 												   .build();
 		Option wordListOption = Option.builder("w").argName("path-to-list-field")
 												   .hasArg()
 												   .longOpt("word-list")
 												   .numberOfArgs(1)
+												   .type(String.class)
 												   .build();
 		Options opts = new Options().addOption(lengthOption)
 									.addOption(delimiterOption)
 									.addOption(wordListOption);
 		DefaultParser parser = new DefaultParser();
-		return parser.parse(opts, args).getOptions();
+		CommandLine cmdLine = parser.parse(opts, args);
+		Object test = cmdLine.getParsedOptionValue("l"); //FIXME debug statement
+		return cmdLine;
 		} catch (ParseException ex) { 
 			// TODO handle this exception with a usage display			
 			return null;
@@ -61,7 +69,7 @@ public class Guard {
 		
 	} // end getOptions
 
-	static String generateArtifact(Option[] options) {
+	static String generateArtifact(CommandLine cmdLine) {
 		return null; // FIXME
 	}
 
@@ -70,17 +78,6 @@ public class Guard {
 	} // end emitArtifact
 
 } // end class
-
-
-
-
-
-
-
-
-
-
-
 
 
 
