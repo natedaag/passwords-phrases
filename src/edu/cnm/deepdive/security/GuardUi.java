0b705179c01c7e008c6038c8a4c08e8834506d03
Nathan <natedaage@yahoo.com>
2017-07-21 14:24:01 -0600

    got box shaped and opening

--- src/edu/cnm/deepdive/security/GuardUi.java
+++ src/edu/cnm/deepdive/security/GuardUi.java
@@ -3,9 +3,12 @@ package edu.cnm.deepdive.security;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GuardUi extends Application {
@@ -13,6 +16,23 @@ public class GuardUi extends Application {
   private static final String STYLES_SHEET = "/styles/application.css";
   private static final String LAYOUTS_RESOURCE = "/layouts/Generate.fxml"; //front slash means "go back to classpath root - package"
   private static final String UI_BUNDLE = "resources/ui"; // no front slash means 'start in current classpath.'
+  private static final String WINDOW_TITLE_KEY = "window.title";
+  
+  @FXML 
+  private VBox passwordOptions;
+  @FXML
+  private VBox passphraseOptions;
+  @FXML
+  private CheckBox upperIncluded;
+  @FXML
+  private CheckBox lowerIncluded;
+  @FXML
+  private CheckBox punctuationIncluded;
+  @FXML
+  private CheckBox ambiguousExcluded;
+  
+  private SecurePasswordGenerator passwordGenerator = new SecurePasswordGenerator();
+  
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -21,7 +41,15 @@ public class GuardUi extends Application {
     Scene scene = new Scene(pane, 400, 400);
     scene.getStylesheets().add(getClass().getResource(STYLES_SHEET).toExternalForm());
     primaryStage.setScene(scene);
+    primaryStage.setTitle(uiBundle.getString(WINDOW_TITLE_KEY));
+//    upperIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+//    lowerIncluded.setSelected(passwordGenerator.isLowerCaseIncluded());
+//    punctuationIncluded.setSelected(passwordGenerator.isPunctuationIncluded());
+//    ambiguousExcluded.setSelected(passwordGenerator.isAmbiguousExcluded());
+
+
     primaryStage.show();
+
   }
 
   public static void main(String[] args) {
